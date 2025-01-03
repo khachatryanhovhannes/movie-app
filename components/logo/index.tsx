9316127f4c5c728aa73f63b38ba1d2392a300eb4
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-03 12:24:31 +0400

    develop: Adding logo

---
+++ components/logo/index.tsx
@@ -0,0 +1,11 @@
+export default function Logo() {
+  return (
+    <div className="flex items-center">
+      <div className="text-red-600 text-6xl">G</div>
+      <div className="flex flex-col text-lg">
+        <span>MOVIES</span>
+        <span>APP</span>
+      </div>
+    </div>
+  );
+}
