eb15ffb4e4eed1c1888e4575aebf16b315ddcd30
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-03 16:56:52 +0400

    develop: Adding top rated section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import { HomeHero, HomeLatest } from "@/components";
+import HomeSections from "@/components/home_sections";
 import { IGenre, IMovie } from "@/models";
-import { getGenres, getPopular } from "@/services/api";
+import { getGenres, getPopular, getTopRated } from "@/services/api";
 
 export default async function Home() {
   const movies: IMovie[] = await getPopular();
   const genres: IGenre[] = await getGenres();
+  const topRated: IMovie[] = await getTopRated();
 
   return (
     <main>
       <HomeHero movies={movies} genres={genres} />
       <HomeLatest />
+      <HomeSections movies={topRated} title="Top Rated" url="" />
     </main>
   );
 }
