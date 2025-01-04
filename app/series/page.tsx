be1a017e1698bcbee4ecc6d6635c2dae3d0c5605
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-04 17:31:35 +0400

    develop: Creating series page

---
+++ app/series/page.tsx
@@ -0,0 +1,7 @@
+export default async function Series() {
+  return (
+    <main>
+      <div className="grid grid-cols-5 py-5 px-5 md:px-8 lg:px-16 xl:px-24 2xl:px-32 text-white">Series</div>
+    </main>
+  );
+}
