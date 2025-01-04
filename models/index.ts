963613a5db9e2eb938f23aee5d2645ea18088b1d
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-04 13:58:16 +0400

    develop: Adding series type

--- models/index.ts
+++ models/index.ts
@@ -19,3 +19,20 @@ export interface IGenre {
   id: number;
   name: string;
 }
+
+export interface ISerie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  first_air_date: string;
+  name: string;
+  vote_average: number;
+  vote_count: number;
+}
