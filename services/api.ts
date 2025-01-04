bf8d9163acc7344897d376b5cb49bb54d9977fc6
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-04 14:08:43 +0400

    develop: Adding top rated api call

--- services/api.ts
+++ services/api.ts
@@ -51,3 +51,13 @@ export const getPopularSeries = async () => {
     throw new Error("Failed to fetch genres");
   }
 };
+
+export const getTopRatedSeries = async () => {
+  try {
+    const res = await instance.get("tv/top_rated?language=en-US&page=1");
+    return res.data.results.slice(0, 4);
+  } catch (err) {
+    console.error(err);
+    throw new Error("Failed to fetch genres");
+  }
+};
