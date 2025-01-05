797190528b9fb502773d5956e727da91d6ea90d0
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-05 12:06:50 +0400

    develop: Adding navbar active

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Link from "next/link";
 import { NAV_ITEMS } from "../../constants/index";
 import Logo from "../logo";
+import { usePathname } from "next/navigation";
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <header>
       <div className="text-white flex justify-between items-center py-5 px-5 md:px-8 lg:px-16 xl:px-24 2xl:px-32">
@@ -9,7 +14,13 @@ export default function Navbar() {
         <nav className="flex gap-8">
           {NAV_ITEMS.map((nav_item) => (
             <Link href={nav_item.href} key={nav_item.label}>
-              {nav_item.label}
+              <span
+                className={`${
+                  pathname === nav_item.href ? "text-red-500" : ""
+                }`}
+              >
+                {nav_item.label}
+              </span>
             </Link>
           ))}
         </nav>
