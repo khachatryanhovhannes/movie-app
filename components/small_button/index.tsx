98202d176586b0d8eb2ab3d054749f030234be33
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-05 14:28:39 +0400

    feature: Adding small button accessablity

--- components/small_button/index.tsx
+++ components/small_button/index.tsx
@@ -5,16 +5,19 @@ interface ISmallButtonProps {
   color: string;
   bgColor: string;
   href: string;
+  ariaLabel?: string;
 }
 
 export default function SmallButton({ ...props }: ISmallButtonProps) {
-  const { text, color, bgColor, href } = props;
+  const { text, color, bgColor, href, ariaLabel } = props;
 
   return (
-    <Link href={href}>
+    <Link href={href} passHref>
       <button
         style={{ color, backgroundColor: bgColor }}
         className="text-xs sm:text-sm lg:text-base py-2 px-4 sm:py-3 sm:px-6 rounded-2xl"
+        aria-label={ariaLabel || text}
+        role="link"
       >
         {text}
       </button>
